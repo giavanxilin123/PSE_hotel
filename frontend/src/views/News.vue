6ddc40cc257910c7f0ecee12d0e37d36ef542ed7
AdamFarouklg <65499327+AdamFarouklg@users.noreply.github.com>
2021-05-27 05:03:42 +0700

    Add files via upload

---
+++ frontend/src/views/News.vue
@@ -0,0 +1,168 @@
+<template>
+  <div>
+    <div class="account-background">
+      <div class="shade-background">OUR NEWS</div>
+    </div>
+    <div class="box-sizing1">
+      <div class="news_background_1">
+        <div class="shade-background_news1">New Website</div>
+      </div>
+      <div class="news_background_2">
+        <div class="shade-background_news2">Follow our Hotel</div>
+      </div>
+      <div class="news_background_3">
+        <div class="shade-background_news3">Relax zone</div>
+      </div>
+      <div class="news_background_4">
+        <div class="shade-background_news4"> Check New Events</div>
+      </div>
+      <div class="news_background_5">
+        <div class="shade-background_news5"> Around us</div>
+      </div>
+      <div class="news_background_6">
+        <div class="shade-background_news6"> Daily Walk</div>
+      </div>
+    
+   </div>
+   </div>
+   
+</template>
+
+
+<style scoped>
+.box-sizing1 {
+  box-sizing: border-box;
+  padding: 30px 300px;
+}
+
+.account-background {
+  background: url(http://www.nicdarkthemes.com/themes/hotel/wp/demo/wp-content/uploads/2017/06/parallax-8.jpg);
+  height: 305px;
+  background-position: center top;
+  background-size: cover;
+}
+
+.news_background_1 {
+  background: url(http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/blog-1.jpg);
+  height: 305px;
+  width: 400px;
+  background-position: center bottom;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.news_background_2 {
+  background-color: rgba(193, 155, 118, 1);
+  height: 300px;
+  width: 400px;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.news_background_3 {
+  background: url(http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/blog-3.jpg);
+  height: 500px;
+  width: 305px;
+  background-position: center;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.news_background_4 {
+  background-color: rgba(0, 0, 0, 1);
+  height: 200px;
+  width: 400px;
+  background-position: center;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.news_background_5 {
+  background: url(http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/blog-2.jpg);
+  height: 305px;
+  width: 400px;
+  background-position: center bottom;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.news_background_6 {
+  background: url(http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/blog-4.jpg);
+  height: 600px;
+  width: 305px;
+  background-position: center;
+  background-size: cover;
+  float: left;
+  margin: 30px;
+}
+.shade-background {
+  background-color: rgba(67, 74, 84, 0.3);
+  height: 305px;
+  font-size: 55px;
+  color: white;
+  line-height: 305px;
+}
+.shade-background_news1 {
+  border:1px solid #000000;
+  height: 510px;
+  font-size: 30px;
+  color: black;
+  line-height: 700px;
+  text-align: left;
+  font-family: "Times New Roman";
+}
+.shade-background_news2 {
+  background-color: rgba(193, 155, 118, 1);
+  height: 150px;
+  font-size: 30px;
+  color: white;
+  line-height: 190px;
+  text-align: center;
+   font-family: "Times New Roman";
+}
+.shade-background_news3 {
+  background-color: rgba(67, 74, 84, 0.3);
+  height: 500px;
+  font-size: 30px;
+  color: white;
+  line-height: 850px;
+  text-align: left;
+   font-family: "papyrus";
+}
+.shade-background_news4 {
+  background-color: solid #000000;
+  height: 200px;
+  width: 400px;
+  font-size: 20px;
+  color: white;
+  text-align: center;
+  line-height: 200px;
+  font-family: "Lucida Console";
+}
+.shade-background_news5 {
+  border:1px solid #000000;
+  height: 450px;
+  font-size: 30px;
+  color: black;
+  line-height: 700px;
+  text-align: left;
+  font-family: "Times New Roman";
+}
+.shade-background_news6 {
+  background-color: rgba(67, 74, 84, 0.3);
+  height: 500px;
+  font-size: 30px;
+  color: white;
+  line-height: 850px;
+  text-align: left;
+   font-family: "papyrus";
+}
+.news_sentence {
+  text-align: center;
+  font-size: 40px;
+  width: 50%;
+  /* padding-right: 200px;
+      padding-left: 200px; */
+}
+
+</style>
