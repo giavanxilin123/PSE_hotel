05f9ff16fe6203d5549e604b73a66590e1b8ad2a
Ryuta <nakamuraxyzal@gmail.com>
2021-05-28 03:18:05 +0700

    fix: add footer UI

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,23 +2,66 @@
   <div id="app">
     <div id="nav">
       <div class="logo">
-        <img src="./assets/hotel-logo.png" alt="">
+        <img src="./assets/hotel-logo.png" alt="" />
       </div>
       <div class="router">
-         <router-link to="/">Home</router-link> 
-         <router-link to="/room">Rooms</router-link>
-         <router-link to="/about-us">About us</router-link>
-         <router-link to="/page">Page</router-link> 
-         <router-link to="/news">News</router-link>
-         <router-link to="/contact">Contact</router-link>
-         <router-link style="color: #c19b76; border-radius: 30%; border: 1px solid #c19b76; padding: 5px 30px; margin-left: 20px" v-if="!user.nickname" to="/authenticate">Login</router-link>
-         <div style="color: white; padding: 5px 30px; margin-left: 20px" v-else>{{user.nickname}}</div>
-         
-      </div> 
+        <router-link to="/">Home</router-link>
+        <router-link to="/room">Rooms</router-link>
+        <router-link to="/about-us">About us</router-link>
+        <router-link to="/page">Page</router-link>
+        <router-link to="/news">News</router-link>
+        <router-link to="/contact">Contact</router-link>
+        <router-link
+          style="
+            color: #c19b76;
+            border-radius: 30%;
+            border: 1px solid #c19b76;
+            padding: 5px 30px;
+            margin-left: 20px;
+          "
+          v-if="!user.nickname"
+          to="/authenticate"
+          >Login</router-link
+        >
+        <div style="color: white; padding: 5px 30px; margin-left: 20px" v-else>
+          {{ user.nickname }}
+        </div>
+      </div>
     </div>
-    <router-view/>
-    <div class ="footer">
+    <router-view />
+    <div class="footer">
       <!-- footer  -->
+      <div class="footer-text">
+        <h2>Phone Support</h2>
+        <h3>+ 01 345 647 745</h3>
+      </div>
+      <div class="footer-text">
+        <h2>Connect With Us</h2>
+        <div class="flex">
+          <img
+            src="http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/icon-4.png"
+            width="20"
+          /><img
+            src="http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/icon-5.png"
+            width="20"
+          /><img
+            src="http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/icon-6.png"
+            width="20"
+          /><img
+            src="http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/icon-7.png"
+            width="20"
+          />
+          <img
+            src="http://www.nicdarkthemes.com/themes/hotel/wp/demo/hotel/wp-content/uploads/sites/2/2017/07/icon-8.png"
+            width="20"
+          />
+        </div>
+      </div>
+      <div class="footer-text">
+        <h2>Newsletter</h2>
+        <el-input placeholder="Please input" v-model="input"> </el-input
+        ><el-button type="primary">Primary Button</el-button>
+      </div>
     </div>
   </div>
 </template>
@@ -26,18 +69,20 @@
 
 <script>
 export default {
-  data(){
-    return {}
+  data() {
+    return {
+      input: "",
+    };
   },
   computed: {
-    user(){
+    user() {
       return this.$store.state.user;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style>
-body{
+body {
   margin: 0;
 }
 #app {
@@ -49,7 +94,7 @@ body{
 }
 
 #nav {
-  padding:25px 100px;
+  padding: 25px 100px;
   background-color: #1c1c1d;
   display: flex;
   justify-content: space-between;
@@ -59,7 +104,7 @@ body{
 #nav a {
   color: #878788;
   text-decoration: none;
-  padding:0 30px;
+  padding: 0 30px;
   font-weight: 500;
   display: flex;
   align-items: center;
@@ -74,12 +119,31 @@ body{
 </style>
 
 <style scoped>
-  .logo img{
-    width: 170px;
-    height: 100%;
-  }
-  .router{
-    display: flex;
-  }
-  
+.logo img {
+  width: 170px;
+  height: 100%;
+}
+.router {
+  display: flex;
+}
+.footer {
+  padding: 25px 100px;
+  background-color: #1c1c1d;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.footer-text {
+  color: #878788;
+  text-decoration: none;
+  padding: 0 30px;
+  font-weight: 500;
+  /* display: flex; */
+  align-items: center;
+}
+.flex {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
 </style>
