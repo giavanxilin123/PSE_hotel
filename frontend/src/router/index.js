368624d2e590b15ec97830dc849b8b8f6e43f81e
Ryuta <nakamuraxyzal@gmail.com>
2021-06-03 19:58:01 +0700

    add: review page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Authenticate from '../views/Authenticate.vue'
 import About_us from '../views/About_us.vue'
+import Review from '../views/Review.vue'
 import News from '../views/News.vue'
 import Contact from '../views/Contact.vue'
 
@@ -27,6 +28,11 @@ const routes = [
     path: '/about-us',
     name: 'About_us',
     component: About_us
+  },
+  {
+    path: '/review',
+    name: 'Review',
+    component: Review
   },
   {
     path: '/news',
