61d69554859847ae821983103ae87252feeca9c1
Ryuta <nakamuraxyzal@gmail.com>
2021-06-11 11:46:18 +0700

    fix: post function for review

--- backend/server/routes/index.js
+++ backend/server/routes/index.js
@@ -1,116 +1,131 @@
-const express = require('express')
-const db = require('../db')
-const router = express.Router()
-const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
-const dotenv = require('dotenv')
-dotenv.config()
+const express = require("express");
+const db = require("../db");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
 
 authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(' ')[1]
-  
-    if (token == null) return res.sendStatus(401)
-  
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET , (err, user) => {
-      console.log(err)
-      if (err) return res.sendStatus(403)
-      req.user = user
-      next()
-    })
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    console.log(err);
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
+
+router.get("/users", authenticateToken, (req, res, next) => {
+  try {
+    db.query(`SELECT * from users`, (err, result) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch {
+    res.status(500).send();
   }
+});
 
-router.get('/users', authenticateToken ,(req, res, next) => {
-    try {
-        db.query(`SELECT * from users`, (err, result) => {
-                if(err){
-                   res.sendStatus(500)
-                }else{
-                    res.json(result)
-                }
-            });
-    }catch{
-        res.status(500).send()
-    }
-})
+router.get("/search", (req, res, next) => {
+  try {
+    db.query(`SELECT * from products`, (err, result) => {
+      if (err) {
+        res.sendStatus(500);
+      } else {
+        res.json(result);
+      }
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
 
-router.get('/search',  (req, res, next) => {
-    try {
-        db.query(`SELECT * from products`, (err, result) => {
-                if(err){
-                   res.sendStatus(500)
-                }else{
-                    res.json(result)
-                }
-            });
-    }catch{
-        res.status(500).send()
-    }
-})
-
-router.post('/register', async (req, res, next) => {
-    try{
-        const salt = await bcrypt.genSalt();
-        const hashedPassword = await bcrypt.hash(req.body.password, salt)
-        const username = req.body.username;
-        const email = req.body.email;
-        const nickname = req.body.nickname;
-        
-        db.query(
-            "INSERT INTO users (username, email, password, nickname) VALUES (?, ?, ?, ?)",
-            [username, email, hashedPassword, nickname],
-            (err, result) =>{
-                res.json(result)
-            }
-        )
-    }catch{
-        res.status(500).send();
-    }
-})
-
-router.post('/login', (req, res, next) => {
+router.post("/register", async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
     const username = req.body.username;
-    const password = req.body.password;
-    
+    const email = req.body.email;
+    const nickname = req.body.nickname;
+
     db.query(
-        `SELECT * FROM users WHERE username = '${username}'`,
-        (err, body) => {
-            if(body.length > 0){
-                bcrypt.compare(
-                    password,
-                    body[0].password,
-                    (err, result) => {
-                        if(result){
-                            const accessToken = jwt.sign({
-                                userId : body[0]._id,
-                                email: body[0].email
-                            }, process.env.ACCESS_TOKEN_SECRET,
-                            {
-                                expiresIn: "1h"
-                            }
-                            )
-                            res.status(200).send({
-                               user: {
-                                    nickname: body[0].nickname,
-                                    username: body[0].username,
-                                    email: body[0].email
-                               },
-                               accessToken : accessToken
-                           })
-                        }
-                        else{
-                            res.status(403).send({message : "Mật khẩu không chính xác"});
-                        }
-                    }
-                )                     
-            }else{
-                res.status(404).send({message : "Tài khoản không tồn tại! "})
-            }
-        }
-    )
-})
+      "INSERT INTO users (username, email, password, nickname) VALUES (?, ?, ?, ?)",
+      [username, email, hashedPassword, nickname],
+      (err, result) => {
+        res.json(result);
+      }
+    );
+  } catch {
+    res.status(500).send();
+  }
+});
 
+router.post("/login", (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  db.query(
+    `SELECT * FROM users WHERE username = '${username}'`,
+    (err, body) => {
+      if (body.length > 0) {
+        bcrypt.compare(password, body[0].password, (err, result) => {
+          if (result) {
+            const accessToken = jwt.sign(
+              {
+                userId: body[0]._id,
+                email: body[0].email,
+              },
+              process.env.ACCESS_TOKEN_SECRET,
+              {
+                expiresIn: "1h",
+              }
+            );
+            res.status(200).send({
+              user: {
+                nickname: body[0].nickname,
+                username: body[0].username,
+                email: body[0].email,
+              },
+              accessToken: accessToken,
+            });
+          } else {
+            res.status(403).send({ message: "Mật khẩu không chính xác" });
+          }
+        });
+      } else {
+        res.status(404).send({ message: "Tài khoản không tồn tại! " });
+      }
+    }
+  );
+});
 
+router.post("/review", async (req, res, next) => {
+  try {
+    const salt = await bcrypt.genSalt();
+
+    const hotelname = req.body.hotelname;
+    const rating = req.body.rating;
+    const title = req.body.title;
+    const review = req.body.review;
+
+    db.query(
+      "INSERT INTO reviews (hotelname, rating, title, review) VALUES (?, ?, ?, ?)",
+      [hotelname, rating, title, review],
+      (err, result) => {
+        res.json(result);
+      }
+    );
+  } catch {
+    res.status(500).send();
+  }
+});
 
 module.exports = router;
--- frontend/src/views/Review.vue
+++ frontend/src/views/Review.vue
@@ -5,37 +5,40 @@
     </div>
     <div class="box">
         <div class="demonstration">Select which hotel you review?</div>
-      <el-select v-model="value" placeholder="Select">
+      <el-select v-model="formReview.hotelname" placeholder="Select">
         <el-option
           v-for="item in options"
-          :key="item.value"
+          :key="item.hotelname"
           :label="item.label"
-          :value="item.value"
+          :hotelname="item.hotelname"
         >
         </el-option>
       </el-select>
       <br><br>
 
       <span class="demonstration">Your overall rating of this property</span><br>
-      <el-rate v-model="value1"></el-rate>
+      <el-rate v-model="formReview.rating"></el-rate>
       <br />
+
       <span class="demonstration">Title of your review </span>
-      <el-input placeholder="Please input" v-model="input"></el-input>
+      <el-input placeholder="Please input" v-model="formReview.title"></el-input>
       <br /><br />
       <span class="demonstration">Your review </span>
       <el-input
         type="textarea"
         :rows="2"
         placeholder="Please input"
-        v-model="textarea"
+        v-model="formReview.review"
       >
       </el-input>
-      <button >SEND NOW</button>
+      <button @click="submit">SEND NOW</button>
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data() {
     return {options: [{
@@ -54,9 +57,30 @@ export default {
           value: 'Option5',
           label: 'Hotel5'
         }],
-        value: ''
+        formReview:{
+          hotelname: '',
+          rating:"",
+          title:"",
+          review:""
+        }
+
+
+
       };
   },
+  computed: {
+    
+  },
+  methods: {
+      submit(){
+        axios.post('http://localhost:3000/api/review', this.formReview)
+        .then(res => {this.alertSuccess(res)})
+        .catch(err => {this.alertErr(err)})
+      }
+    
+      
+  }
+  
 };
 </script>
 <style scoped>
